ed3f8f671dcbd41a3258994829066b5c73f91c3a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-07-24 23:41:42 +0900

    Initial commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by joe on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
